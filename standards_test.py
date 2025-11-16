806350852b1d2c62b695b868de421f57c9164e6c
Ritul Patel <143929587+ritul-patel@users.noreply.github.com>
2025-11-16 20:26:49 +0000

    Test 8: standards (no type hints, formatting)

---
+++ standards_test.py
@@ -0,0 +1,9 @@
+# STANDARDS_TEST: no type hints, inconsistent formatting
+def add(a,b):
+ return a + b
+
+def long_func():
+    '''should have detailed docstring'''
+    x=1
+    y=2
+    return x+y
